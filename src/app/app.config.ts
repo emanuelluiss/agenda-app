7af98eed85276cbc3db65227acdd5b906cd075de
Emanuel Luiz da Silva Matos Pinto <emanuel.matos@saude.ce.gov.br>
2025-12-04 02:29:57 -0300

    feat: configuração inicial

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,34 @@
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
+import { MockBackendInterceptor } from './core/interceptors/mock-backend.interceptor';
+import { LogService } from './core/services/log.service';
+import { NotificationService } from './core/services/notification.service';
+import { AgendaPreset } from './core/themes/agenda.preset';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withHashLocation()),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()),
+    providePrimeNG({
+        theme: {
+            preset: AgendaPreset,
+            options: {
+                darkModeSelector: false,
+            }
+        }
+    }),
+    MessageService,
+    ConfirmationService,
+    { provide: HTTP_INTERCEPTORS, useClass: MockBackendInterceptor, multi: true },
+    LogService,
+    NotificationService
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+  path: '',
+  loadChildren: () => import('./features/agenda/agenda.routes').then(m => m.AGENDA_ROUTES)
+  },
+
+  // Rota Coringa (Wildcard): Se digitar qualquer url maluca, volta para o início
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
